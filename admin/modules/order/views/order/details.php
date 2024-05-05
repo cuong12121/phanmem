a063d7994fb36ddb0290b5716e0a89d4ac1d6c04
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2024-05-05 13:52:01 +0700

    update code

--- admin/modules/order/views/order/details.php
+++ admin/modules/order/views/order/details.php
@@ -1,4 +1,14 @@
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
 
+<!-- jQuery library -->
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
+
+<!-- Popper JS -->
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
  <style type="text/css">
     table, th, td{
         border:1px solid #868585;
@@ -28,6 +38,7 @@
             <th>Tên shop</th>
             <th>Mã shop</th>
             <th>Số lượng</th>
+            <th>Id đơn hàng</th>
             <th>Thành tiền</th>
         </tr>
 
@@ -52,6 +63,7 @@
             <td><?= $value->shop_name  ?></td>
             <td><?= $value->shop_code  ?></td>
             <td><?= $value->count  ?></td>
+            <td><?= $value->record  ?></td>
             <td><?= $value->total_price  ?>đ</td>
         </tr>
 
@@ -62,3 +74,21 @@
         ?>
        
     </table>
+
+    <nav aria-label="Page navigation example">
+        <ul class="pagination">
+            <li class="page-item">
+                <a class="page-link" href="#" aria-label="Previous">
+                <span aria-hidden="true">&laquo;</span>
+                </a>
+            </li>
+            <li class="page-item"><a class="page-link" href="#">1</a></li>
+            <li class="page-item"><a class="page-link" href="#">2</a></li>
+            <li class="page-item"><a class="page-link" href="#">3</a></li>
+            <li class="page-item">
+                <a class="page-link" href="#" aria-label="Next">
+                <span aria-hidden="true">&raquo;</span>
+                </a>
+            </li>
+        </ul>
+    </nav>
