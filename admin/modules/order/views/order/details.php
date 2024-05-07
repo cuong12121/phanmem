dfb4c1a40375cc26ca3aa2ec8dcd0b0a8f428404
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2024-05-07 08:21:55 +0700

    Update details.php

--- admin/modules/order/views/order/details.php
+++ admin/modules/order/views/order/details.php
@@ -125,12 +125,12 @@
             <td><?=  number_format((float)$value->total_price, 0, ',', '.') ?>đ</td>
             <td class="return">
                 <?php
-                    if($now=== $date_package):
+                    if($now=== $date_package){
                 ?>    
                 <a href="https://dienmayai.com/admin/order/detail/search?search=<?= $value-> ?>&active=0">Hoàn đơn</a>
 
                 <?php 
-                    endif;
+                   }
                 ?>
             </td>
         </tr>
