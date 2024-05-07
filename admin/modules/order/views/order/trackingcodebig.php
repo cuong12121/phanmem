6e9eb36e1762f0571c95cc0572de458148046d29
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2024-05-07 14:22:00 +0700

    Update trackingcodebig.php

--- admin/modules/order/views/order/trackingcodebig.php
+++ admin/modules/order/views/order/trackingcodebig.php
@@ -9,6 +9,9 @@
 
 <!-- Latest compiled JavaScript -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+
  <style type="text/css">
     table, th, td{
         border:1px solid #868585;
