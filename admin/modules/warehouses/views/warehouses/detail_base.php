4d8a98eb6e54a3b92bb8acf39fd7628a6a5600ba
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2024-05-09 22:06:36 +0700

    Update detail_base.php

--- admin/modules/warehouses/views/warehouses/detail_base.php
+++ admin/modules/warehouses/views/warehouses/detail_base.php
@@ -1,6 +1,18 @@
 <table cellspacing="1" class="admintable">
 	<?php
 	TemplateHelper::dt_edit_text(FSText :: _('Name'),'name',@$data -> name);
+
+	?>
+	<label for="parent">Kho cha</label>
+
+	<select name="cars" id="cars">
+
+	  	<option value="0">Kho Hà Nội</option>
+	  	<option value="0">Kho Sài Gòn</option>
+	  	
+	</select>
+
+	<?php
 	// TemplateHelper::dt_edit_text(FSText :: _('Mô tả'),'summary',@$data -> summary,'',100,5,0);
 	TemplateHelper::dt_checkbox(FSText ::_('Published'),'published',@$data -> published,1);
 	?>
