498933304ea6c3ec1568dc8dc063be35950319e2
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2024-05-24 06:26:13 +0700

    Update upload.php

--- admin/modules/order/controllers/upload.php
+++ admin/modules/order/controllers/upload.php
@@ -189,7 +189,7 @@
 					        	$html .= '<a target="_blank" style="color: rgba(255, 153, 0, 0.79);" href="'.$convert_url_vietnam.'">'.$base_name.'</a><br/>';
 					        }
 					        else{
-					        	 $html .= '<a target="_blank" style="color: red;" href="javascript:void(0)">'.PATH_BASE.$path.$base_name.'</a><br/>';
+					        	 $html .= '<a target="_blank" style="color: red;" href="javascript:void(0)">'.$convert_url_vietnam.'</a><br/>';
 					        }
 					        
 					       
