14817955e7b8c37ffe44b44f3f7d7fc6d3501316
cuong12121 <cuongdao12121@gmail.com>
2024-05-24 09:28:31 +0700

    Update upload.php

--- admin/modules/order/models/upload.php
+++ admin/modules/order/models/upload.php
@@ -120,37 +120,32 @@
 			return $query;
 		}
 
-		function convertSlug($title)
+		function convertSlug($str)
         {
        
-	        $replacement = '-';
-	        $map = array();
-	        $quotedReplacement = preg_quote($replacement, '/');
-	        $default = array(
-	            '/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ|À|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ|å/' => 'a',
-	            '/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ|È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ|ë/' => 'e',
-	            '/ì|í|ị|ỉ|ĩ|Ì|Í|Ị|Ỉ|Ĩ|î/' => 'i',
-	            '/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ|Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ|ø/' => 'o',
-	            '/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ|Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ|ů|û/' => 'u',
-	            '/ỳ|ý|ỵ|ỷ|ỹ|Ỳ|Ý|Ỵ|Ỷ|Ỹ/' => 'y',
-	            '/đ|Đ/' => 'd',
-	            '/ç/' => 'c',
-	            '/ñ/' => 'n',
-	            '/ä|æ/' => 'ae',
-	            '/ö/' => 'oe',
-	            '/ü/' => 'ue',
-	            '/Ä/' => 'Ae',
-	            '/Ü/' => 'Ue',
-	            '/Ö/' => 'Oe',
-	            '/ß/' => 'ss',
-	            '/[^\s\p{Ll}\p{Lm}\p{Lo}\p{Lt}\p{Lu}\p{Nd}]/mu' => ' ',
-	            '/\\s+/' => $replacement,
-	            sprintf('/^[%s]+|[%s]+$/', $quotedReplacement, $quotedReplacement) => '',
-	        );
-	        //Some URL was encode, decode first
-	        $title = urldecode($title);
-	        $map = array_merge($map, $default);
-	        return strtolower(preg_replace(array_keys($map), array_values($map), $title));
+	        // Mảng chứa các ký tự có dấu
+		    $unicode = array(
+		        'a'=>'á|à|ả|ã|ạ|ă|ắ|ằ|ẳ|ẵ|ặ|â|ấ|ầ|ẩ|ẫ|ậ',
+		        'd'=>'đ',
+		        'e'=>'é|è|ẻ|ẽ|ẹ|ê|ế|ề|ể|ễ|ệ',
+		        'i'=>'í|ì|ỉ|ĩ|ị',   
+		        'o'=>'ó|ò|ỏ|õ|ọ|ô|ố|ồ|ổ|ỗ|ộ|ơ|ớ|ờ|ở|ỡ|ợ',   
+		        'u'=>'ú|ù|ủ|ũ|ụ|ư|ứ|ừ|ử|ữ|ự',   
+		        'y'=>'ý|ỳ|ỷ|ỹ|ỵ',   
+		        'A'=>'Á|À|Ả|Ã|Ạ|Ă|Ắ|Ằ|Ẳ|Ẵ|Ặ|Â|Ấ|Ầ|Ẩ|Ẫ|Ậ',
+		        'D'=>'Đ',
+		        'E'=>'É|È|Ẻ|Ẽ|Ẹ|Ê|Ế|Ề|Ể|Ễ|Ệ',
+		        'I'=>'Í|Ì|Ỉ|Ĩ|Ị',
+		        'O'=>'Ó|Ò|Ỏ|Õ|Ọ|Ô|Ố|Ồ|Ổ|Ỗ|Ộ|Ơ|Ớ|Ờ|Ở|Ỡ|Ợ',
+		        'U'=>'Ú|Ù|Ủ|Ũ|Ụ|Ư|Ứ|Ừ|Ử|Ữ|Ự',
+		        'Y'=>'Ý|Ỳ|Ỷ|Ỹ|Ỵ',
+		    );
+
+		    // Thay thế các ký tự có dấu bằng ký tự không dấu
+		    foreach($unicode as $nonUnicode=>$uni) {
+		        $str = preg_replace("/($uni)/i", $nonUnicode, $str);
+		    }
+		    return $str;
 
 	    }
 
