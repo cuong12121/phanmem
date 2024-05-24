5814470fccd035f3262496cca44b0f878c00cf1e
cuong12121 <cuongdao12121@gmail.com>
2024-05-24 09:13:29 +0700

    update

--- admin/modules/order/controllers/upload.php
+++ admin/modules/order/controllers/upload.php
@@ -114,6 +114,40 @@
 			}
 		}
 
+		function convertSlug($title)
+        {
+       
+	        $replacement = '-';
+	        $map = array();
+	        $quotedReplacement = preg_quote($replacement, '/');
+	        $default = array(
+	            '/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ|À|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ|å/' => 'a',
+	            '/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ|È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ|ë/' => 'e',
+	            '/ì|í|ị|ỉ|ĩ|Ì|Í|Ị|Ỉ|Ĩ|î/' => 'i',
+	            '/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ|Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ|ø/' => 'o',
+	            '/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ|Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ|ů|û/' => 'u',
+	            '/ỳ|ý|ỵ|ỷ|ỹ|Ỳ|Ý|Ỵ|Ỷ|Ỹ/' => 'y',
+	            '/đ|Đ/' => 'd',
+	            '/ç/' => 'c',
+	            '/ñ/' => 'n',
+	            '/ä|æ/' => 'ae',
+	            '/ö/' => 'oe',
+	            '/ü/' => 'ue',
+	            '/Ä/' => 'Ae',
+	            '/Ü/' => 'Ue',
+	            '/Ö/' => 'Oe',
+	            '/ß/' => 'ss',
+	            '/[^\s\p{Ll}\p{Lm}\p{Lo}\p{Lt}\p{Lu}\p{Nd}]/mu' => ' ',
+	            '/\\s+/' => $replacement,
+	            sprintf('/^[%s]+|[%s]+$/', $quotedReplacement, $quotedReplacement) => '',
+	        );
+	        //Some URL was encode, decode first
+	        $title = urldecode($title);
+	        $map = array_merge($map, $default);
+	        return strtolower(preg_replace(array_keys($map), array_values($map), $title));
+
+	    }
+
 		function prints()
 		{
 			$model = $this -> model;
@@ -184,7 +218,7 @@
 					        $html .= '<a target="_blank" style="color: rgba(255, 153, 0, 0.79);" href="'.$url.'">'.$base_name.'</a><br/>';
 					    }
 					    else{
-					    	$convert_url_vietnam = URL_ROOT.$path.urlencode($base_name);
+					    	$convert_url_vietnam = $this->convertSlug(URL_ROOT.$path.$base_name);
 					        if(file_exists($convert_url_vietnam)){
 					        	$html .= '<a target="_blank" style="color: rgba(255, 153, 0, 0.79);" href="'.$convert_url_vietnam.'">'.$base_name.'</a><br/>';
 					        }
