868e250aee380506dcf81726a9ddaba58e6adff9
cuong12121 <cuongdao12121@gmail.com>
2024-05-24 09:17:11 +0700

    Update upload.php

--- admin/modules/order/controllers/upload.php
+++ admin/modules/order/controllers/upload.php
@@ -218,7 +218,7 @@
 					        $html .= '<a target="_blank" style="color: rgba(255, 153, 0, 0.79);" href="'.$url.'">'.$base_name.'</a><br/>';
 					    }
 					    else{
-					    	$convert_url_vietnam = $this->convertSlug(URL_ROOT.$path.$base_name);
+					    	$convert_url_vietnam = convertSlug(URL_ROOT.$path.$base_name);
 					        if(file_exists($convert_url_vietnam)){
 					        	$html .= '<a target="_blank" style="color: rgba(255, 153, 0, 0.79);" href="'.$convert_url_vietnam.'">'.$base_name.'</a><br/>';
 					        }
