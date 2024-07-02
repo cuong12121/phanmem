aea7615472bb715566e94302fc9e7f8969aae864
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2024-07-02 17:14:56 +0700

    update code

--- admin/modules/order/controllers/order.php
+++ admin/modules/order/controllers/order.php
@@ -146,6 +146,7 @@
 	                
 	            )
 	        ));
+	        
 
 	        // Send the request
 	        $response = file_get_contents('https://api.dienmayai.com/api/search-data-order-details?search='.$search.'&user_package_id='.$user_id.'&active='.$active, FALSE, $context);
