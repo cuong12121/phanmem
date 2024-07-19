6100a9207cc40ff2b14ffc2b4101f76383e9455a
cuong12121 <cuongdao12121@gmail.com>
2024-07-19 15:00:11 +0700

    Update upload.php

--- admin/modules/order/models/upload.php
+++ admin/modules/order/models/upload.php
@@ -1328,6 +1328,14 @@
 					$InputFile  = PATH_BASE.'files/orders/'.$cyear.'/'.$cmonth.'/'.$cday.'/'.$item_file_pdf_name;
 					$OutputFile = PATH_BASE.'files/orders/'.$cyear.'/'.$cmonth.'/'.$cday.'/'.str_replace('.pdf','_cv.pdf',$item_file_pdf_name);
 
+					$check_pdf_text = trim(PdfToText::getText($InputFile));
+
+					if(empty($check_pdf_text)){
+						$msg = 'file pdf với tên là '.$item_file_pdf_name. ' đang là định dạng pdf ảnh, cần chuyển sang định dạng pdf text!' ;
+						setRedirect($link,$msg,'error');
+						return false;
+					}
+
 					if($_SERVER['SERVER_ADDR'] == '127.0.0.1'){ // trên local
 						$cmd = "gswin64 -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dNOPAUSE -dQUIET -dBATCH -sOutputFile=".$OutputFile." ".$InputFile;
 					}else{ //trên server linux
