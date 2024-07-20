7c9c3f3ed1348002178f3f47e5e48ee8bdeea348
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2024-07-20 18:08:23 +0700

    Update upload.php

--- admin/modules/order/models/upload.php
+++ admin/modules/order/models/upload.php
@@ -503,7 +503,7 @@
 			
 			$text = PdfToText::getText($path);
 
-			var_dump($text);
+			return $text;
 		}
 
 
@@ -1340,22 +1340,19 @@
 					$InputFile  = PATH_BASE.'files/orders/'.$cyear.'/'.$cmonth.'/'.$cday.'/'.$item_file_pdf_name;
 					$OutputFile = PATH_BASE.'files/orders/'.$cyear.'/'.$cmonth.'/'.$cday.'/'.str_replace('.pdf','_cv.pdf',$item_file_pdf_name);
 
-					$parser = new \Smalot\PdfParser\Parser();
-					$pdfs = $parser->parseFile($InputFile);
+					$text = $th->showPDFText($path);
 
 					
-
-
-
-					// if($platform_id !=6 && $pdf == "") {
-
-					// 	$link = FSRoute::_('index.php?module=order&view=upload&task=edit&id='.$id);
-					// 	$msg = 'file pdf với tên là '.$item_file_pdf_name. ' đang là định dạng pdf ảnh, cần chuyển sang định dạng pdf text!' ;
-					// 	setRedirect($link,$msg,'error');
-					// 	return false;
-					// }	
+					$text_pdf_check = $this->showPDFText($InputFile);
 
 					
+					if($platform_id !=6 && $text_pdf_check == "") {
+
+						$link = FSRoute::_('index.php?module=order&view=upload&task=edit&id='.$id);
+						$msg = 'file pdf với tên là '.$item_file_pdf_name. ' đang là định dạng pdf ảnh, cần chuyển sang định dạng pdf text!' ;
+						setRedirect($link,$msg,'error');
+						return false;
+					}	
 
 					if($_SERVER['SERVER_ADDR'] == '127.0.0.1'){ // trên local
 						$cmd = "gswin64 -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dNOPAUSE -dQUIET -dBATCH -sOutputFile=".$OutputFile." ".$InputFile;
