f8de86e1e1ffd0f8f601bff9ad8610bc03243c17
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2024-07-21 15:07:21 +0700

    Update upload.php

--- admin/modules/order/models/upload.php
+++ admin/modules/order/models/upload.php
@@ -182,7 +182,7 @@
             // }
 
              if(empty($b[0])){
-                preg_match_all('/[A-Z-0-9]{1}[A-Za-z0-9]{1}[A-Za-z0-9]{1}[A-Za-z0-9]{1}+\s*-\s*[A-Za-z0-9][A-Za-z0-9]+\s*-\s*[A-Za-z0-9][A-Za-z0-9]+\s*-\s*[A-Za-z0-9][A-Za-z0-9][A-Za-z0-9]/', $content, $b);
+                preg_match_all('/[A-Z-0-9]{1}[A-Za-z0-9]{1}[A-Za-z0-9]{1}[A-Za-z0-9]{1}+\s*-\s*[A-Za-z0-9][A-Za-z0-9]/', $content, $b);
             }
 
             // if(empty($b[0])){
