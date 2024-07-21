f70b0cc8c6bd03908a6659c7838fcfab292c03b2
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2024-07-21 15:09:10 +0700

    Update upload.php

--- admin/modules/order/models/upload.php
+++ admin/modules/order/models/upload.php
@@ -171,6 +171,10 @@
 		}   
 
 		function convertContenttiktok($content){
+
+			var_dump($content);
+
+			die;
             
           
             
@@ -182,7 +186,7 @@
             // }
 
              if(empty($b[0])){
-                preg_match_all('/[A-Z-0-9]{1}[A-Za-z0-9]{1}[A-Za-z0-9]{1}[A-Za-z0-9]{1}+\s*-\s*[A-Za-z0-9][A-Za-z0-9]/', $content, $b);
+                preg_match_all('/[A-Z-0-9]{1}[A-Za-z0-9]{1}[A-Za-z0-9]{1}[A-Za-z0-9]{1}+\s*-\s*[A-Za-z0-9][A-Za-z0-9]+\s*-\s*[A-Za-z0-9][A-Za-z0-9]+\s*-\s*[A-Za-z0-9][A-Za-z0-9][A-Za-z0-9]+\s*-\s*[A-Za-z0-9][A-Za-z0-9]+\s*-\s*[[0-9]{1,3}|0]/', $content, $b);
             }
 
             // if(empty($b[0])){
