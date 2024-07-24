56abd84bb607d44ce7a5669dd7ab0882fd453bba
cuong12121 <cuongdao12121@gmail.com>
2024-07-24 15:03:54 +0700

    Update upload.php

--- admin/modules/order/controllers/upload.php
+++ admin/modules/order/controllers/upload.php
@@ -179,16 +179,15 @@
 
 			$data_pdf = $this->dataPDF($file_ar_pdf, $platform_id);
 
-			var_dump($data_pdf['sku']);
+			
+			if($platform_id ==2){
+				$data_pdfs = $data_pdf;
+			}
+			else{
+				$data_pdfs['sku'] = array_merge(...array_values($data_pdf['sku']));
 
-			die;
-
-			$data_pdfs['sku'] = array_merge(...array_values($data_pdf['sku']));
-
-			$data_pdfs['mavandon'] = array_merge(...array_values($data_pdf['mavandon']));
-
-
-
+				$data_pdfs['mavandon'] = array_merge(...array_values($data_pdf['mavandon']));
+			}
 
 			$result = $this->resultcheckPdfAndEx($data, $data_pdfs);
 
