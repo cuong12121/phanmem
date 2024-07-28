db4dc3991533f25df07365da515767ef82418df6
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2024-07-28 20:56:12 +0700

    Update list-err.php

--- admin/modules/order/views/order/list-err.php
+++ admin/modules/order/views/order/list-err.php
@@ -218,7 +218,7 @@
 
                     }
                     $link_pdf_href = '<a href="'.$link_pdf.'" target="blank">'.basename($link_pdf).'</a>';
-                     array_push($file_pdf_rep1, $link_pdf_href.!empty(checkrepair($link_pdf))?$link_pdf_href.'_'.checkrepair($link_pdf):$link_pdf_href);
+                     array_push($file_pdf_rep1, $link_pdf_href.checkrepair($link_pdf)===''?$link_pdf_href.'_'.checkrepair($link_pdf):$link_pdf_href);
 
                 }
 
