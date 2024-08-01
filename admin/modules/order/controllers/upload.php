fd8270f4d37c516c20bd0d9ffd349e82a29b49ba
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2024-08-01 21:44:24 +0700

    Update upload.php

--- admin/modules/order/controllers/upload.php
+++ admin/modules/order/controllers/upload.php
@@ -543,7 +543,7 @@
 			preg_match_all('/Mã đơn hàng:\s*([A-Z0-9]+)/', $content, $maVanDonMatches);
             	$maVanDon = isset($maVanDonMatches[1]) ? $maVanDonMatches[1] : null;
 
-            var_dump($maVanDonMatches);
+            var_dump($maVanDon);
 
             die;
 		}
