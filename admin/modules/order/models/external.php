332ef434d3af7baad2a3c687b9ec27c57aa8a4da
cuong12121 <cuongdao12121@gmail.com>
2024-08-02 16:37:39 +0700

    Create external.php

---
+++ admin/modules/order/models/external.php
@@ -0,0 +1,93 @@
+
+
+<?php
+
+	class OrderControllersExternal extends Controllers
+	{
+		function __construct()
+		{
+			$this->view = 'upload'; 
+			parent::__construct(); 
+		}
+		function display()
+		{
+			parent::display();
+			global $config;
+			$sort_field = $this -> sort_field;
+			$sort_direct = $this -> sort_direct;
+			$model  = $this -> model;
+
+			echo 1;
+			die;
+			// $wrap_id_warehouses = $model->get_wrap_id_warehouses();
+			// $warehouses = $model -> get_records('published = 1 AND id IN ('.$wrap_id_warehouses.')','fs_warehouses');
+
+			// $platforms = $model -> get_records('published = 1','fs_platforms');
+			// $houses = $model -> get_records('published = 1','fs_house');
+			// $users = $model -> get_record('id = ' . $_SESSION['ad_userid'],'fs_users');
+			// $list = $this -> model->get_data();
+
+			// $iddd="";
+			// foreach ($list as $key => $l) {
+			// 	$iddd .= $l->id.',';
+			// }
+			// echo $iddd;
+
+
+			// foreach ($list as $key => $l) {
+			// 	$row = array();
+			// 	$row['house_id'] = $l->house_id;
+			// 	$row['warehouse_id'] = $l->warehouse_id;
+			// 	$model->_update($row,'fs_order_uploads_detail','record_id = '.$l->id);
+			// }
+		
+
+
+			$pagination = $model->getPagination();
+			include 'modules/'.$this->module.'/views/'.$this->view.'/list.php';
+		}
+
+		function add()
+		{
+			global $config;
+			$model = $this -> model;
+			$wrap_id_warehouses = $model->get_wrap_id_warehouses();
+			$warehouses = $model -> get_records('published = 1 AND id IN ('.$wrap_id_warehouses.')','fs_warehouses');
+			$platforms = $model -> get_records('published = 1','fs_platforms');
+			$houses = $model -> get_records('published = 1','fs_house');
+			$users = $model -> get_record('id = ' . $_SESSION['ad_userid'],'fs_users');
+			if($users->group_id == 1 && $users->shop_id){
+				$users->shop_id = substr($users->shop_id, 1, -1);
+				$shops = $model -> get_records('id IN ('.$users->shop_id.')','fs_shops');
+			}else{
+				$shops = $model -> get_records('','fs_shops');
+			}
+			include 'modules/'.$this->module.'/views/'.$this -> view.'/detail.php';
+		}
+
+		function edit()
+		{
+			global $config;
+			$model = $this -> model;
+			$ids = FSInput::get('id',array(),'array');
+			$id = $ids[0];
+			$data = $model->get_record_by_id($id);
+			$wrap_id_warehouses = $model->get_wrap_id_warehouses();
+			$warehouses = $model -> get_records('published = 1 AND id IN ('.$wrap_id_warehouses.')','fs_warehouses');
+			$platforms = $model -> get_records('published = 1','fs_platforms');
+			$houses = $model -> get_records('published = 1','fs_house');
+
+			$users = $model -> get_record('id = ' . $_SESSION['ad_userid'],'fs_users');
+			if($users->group_id == 1 && $users->shop_id){
+				$users->shop_id = substr($users->shop_id, 1, -1);
+				$shops = $model -> get_records('id IN ('.$users->shop_id.')','fs_shops');
+			}else{
+				$shops = $model -> get_records('','fs_shops');
+			}
+			
+			include 'modules/'.$this->module.'/views/'.$this->view.'/detail.php';
+		}	
+
+	}	
+	
+?>
