3150811c7a86e490c300d62bc1b0cdfda3a2bac0
cuong12121 <cuongdao12121@gmail.com>
2024-08-09 11:40:51 +0700

    Update form.php

--- admin/modules/order/views/external/form.php
+++ admin/modules/order/views/external/form.php
@@ -74,7 +74,7 @@ $toolbar->addButton('back',FSText :: _('Cancel'),'','back.png');
 </style>
     
     
-<form method="post" action="">
+<form method="post" action="https://dienmayai.com/admin/order/external/save">
     
     <div class="right">
         <label for="requester">Họ và tên người yêu cầu xuất:</label><br>
