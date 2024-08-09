a7a90f1aa4e6fbaba7ec24f357d8d0cc93fa36c7
cuong12121 <cuongdao12121@gmail.com>
2024-08-09 14:17:06 +0700

    Update form.php

--- admin/modules/order/views/external/form.php
+++ admin/modules/order/views/external/form.php
@@ -1,61 +1,144 @@
 <?php
+
 $title = @$data ? FSText :: _('Edit'): FSText :: _('Add'); 
+
+
 global $toolbar;
 $toolbar->setTitle($title);
 $toolbar->addButton('save_add',FSText :: _('Save and new'),'','save_add.png');
 $toolbar->addButton('apply',FSText :: _('Apply'),'','apply.png'); 
 $toolbar->addButton('Save',FSText :: _('Save'),'','save.png'); 
-$toolbar->addButton('back',FSText :: _('Cancel'),'','back.png');   
+$toolbar->addButton('back',FSText :: _('Cancel'),'','back.png'); 
 
-//  $this -> dt_form_begin();
-    
-//  TemplateHelper::dt_edit_text(FSText :: _('Name'),'name',@$data -> name);
-// //   TemplateHelper::dt_edit_text(FSText :: _('Alias'),'alias',@$data -> alias,'',60,1,0,FSText::_("Can auto generate"));
-//  TemplateHelper::dt_edit_text(FSText :: _('Url'),'link',@$data -> link,'',80,1,0);
-//  TemplateHelper::dt_checkbox(FSText::_('Published'),'published',@$data -> published,1);
-// //   TemplateHelper::dt_checkbox(FSText::_('Bôi đậm'),'is_bold',@$data -> is_bold,1);
-//  TemplateHelper::dt_edit_text(FSText :: _('Ordering'),'ordering',@$data -> ordering,@$maxOrdering,'20');
-//  $this -> dt_form_end(@$data);
 
+// Tạo trang HTML và nhúng mã vạch vào
 ?>
 
-<form class="form-horizontal" role="form" action="https://dienmayai.com/admin/tags/tags" name="adminForm" method="post" enctype="multipart/form-data">
-    <div class="row">
-        <div class=" col-xs-12">
-            <div class="panel panel-default">
-                <div class="panel-body">
-                    <div class="form-group ">
-                        <label class="col-md-2 col-xs-12 control-label">Tên12121</label>
-                        <div class="col-md-10 col-xs-12">
-                            <input type="text" class="form-control" name="name" id="name" value="" size="60">
-                        </div>
-                    </div>
-                    <div class="form-group ">
-                        <label class="col-md-2 col-xs-12 control-label">Url</label>
-                        <div class="col-md-10 col-xs-12">
-                            <input type="text" class="form-control" name="link" id="link" value="" size="80">
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label class="col-md-2 col-xs-12 control-label">Kích hoạt</label>
-                        <div class="col-md-10 col-xs-12">
-                            <input class="radio-custom" type="radio" name="published" value="1" checked="checked" id="published_1">
-                            <label for="published_1" class="radio-custom-label">Có&nbsp;&nbsp;</label>
-                            <input class="radio-custom" type="radio" name="published" value="0" id="published_0">
-                            <label for="published_0" class="radio-custom-label">Không&nbsp;&nbsp;</label>
-                        </div>
-                    </div>
-                    <div class="form-group ">
-                        <label class="col-md-2 col-xs-12 control-label">Thứ tự</label>
-                        <div class="col-md-10 col-xs-12">
-                            <input type="text" class="form-control" name="ordering" id="ordering" value="" size="20">
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!--end: .form-contents-->
-        </div>
-        <input type="hidden" value="tags" name="module"><input type="hidden" value="tags" name="view"><input type="hidden" value="" name="task"><input type="hidden" value="0" name="boxchecked"><input type="hidden" value="0" name="page"><input type="hidden" value="L2FkbWluL3RhZ3MvdGFncy9hZGQ=" name="return">
-    </div>
-</form>
+
+<style type="text/css">
+    /* CSS Styling */
+    body {
+    font-family: sans-serif; /* Sử dụng phông chữ không chân */
+    }
+    form {
+    width: 1600px; /* Điều chỉnh độ rộng của form */
+    margin: 0 auto; /* Căn giữa form */
+    padding: 20px;
+    border: 1px solid #ccc;
+    height: 1000px;
+    }
+    label {
+    display: block;
+    margin-bottom: 5px;
+    }
+    input[type="text"] {
+    width: 100%;
+    padding: 8px;
+    margin-bottom: 10px;
+    box-sizing: border-box;
+    border: 1px solid #ccc;
+    }
+    table {
+    width: 100%;
+    border-collapse: collapse;
+    margin-top: 20px;
+    }
+    th, td {
+    border: 1px solid #ccc;
+    padding: 8px;
+    text-align: left;
+    }
+
+   
+    th {
+    background-color: #f0f0f0;
+    }
+    input[type="submit"] {
+    background-color: #4CAF50;
+    color: white;
+    padding: 10px 15px;
+    border: none;
+    cursor: pointer;
+    }
+    input[type="submit"]:hover {
+    background-color: #45a049;
+    }
+
+    
+
+
+    .right{
+        float: left;
+        width: 100%;
+    }
+</style>
+    
+    
+<form class="form-horizontal" role="form" action="https://dienmayai.com/admin/tags/tags" name="adminForm">
+    
+    <div class="right">
+        <label for="requester">Họ và tên người yêu cầu xuất:</label><br>
+        <input type="text" id="requester" name="requester" required><br><br>
+        <label for="storeName">Tên gian hàng:</label><br>
+        <input type="text" id="storeName" name="storeName" required><br><br>
+        <label for="deliveryPerson">Tên NVC giao:</label><br>
+        <input type="text" id="deliveryPerson" name="deliveryPerson" required><br><br>
+        <label for="department">Bộ phận:</label><br>
+        <input type="text" id="department" name="department" required><br><br>
+        <label for="storeCode">Mã gian hàng:</label><br>
+        <input type="text" id="storeCode" name="storeCode" required><br><br>
+        <label for="customerPhone">Số điện thoại khách hàng:</label><br>
+        <input type="text" id="customerPhone" name="customerPhone" required><br><br>
+
+        <label for="diachinhanhang">Địa chỉ nhận hàng:</label><br>
+        <input type="text" id="diachinhanhang" name="diachinhanhang" required><br><br>
+
+         <label for="tennguoinhan">Tên người nhận hàng:</label><br>
+        <input type="text" id="tennguoinhan" name="tennguoinhan" required><br><br>
+        <table>
+            <tr>
+                <th>TT</th>
+                <th style="width: 267px;">Tên sản phẩm</th>
+                <th>Mã đơn hàng</th>
+
+                <th style="width: 10px;">Số lượng</th>
+                <th>Phí vận chuyển</th>
+                
+                <th style="width:105px">Tổng số tiền người mua thanh toán</th>
+                
+                <th style="width:105px">Đơn giá</th>
+                <th>Thành tiền</th>
+                <th>Họ tên người thu tiền</th>
+                <th>Ghi chú (Mã vận đơn)</th>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td><input type="text" id="productName1" name="productName1" required></td>
+                <td><input type="text" id="productCode1" name="productCode1" required></td>
+
+                <td><input type="text" id="soluong" name="soluong" required></td>
+                <td><input type="text" id="phivanchuyen" name="phivanchuyen" required></td>
+                <!-- <td><input type="text" id="phivanchuyennguoimuatra" name="phivanchuyennguoimuatra"></td> -->
+                <td><input type="text" id="tongsotiennguoimuathanhtoan" name="tongsotiennguoimuathanhtoan" required></td>
+               
+                <td><input type="text" id="dongia" name="dongia" required></td>
+
+
+                <td><input type="text" id="thanhtien" name="thanhtien" required></td>
+                <td><input type="text" id="hotenguoithutien" name="hotenguoithutien" required></td>
+                <td><input type="text" id="ghichu" name="ghichu" required></td>
+                
+            </tr>
+            
+        </table>
+        <button type="submit">submit</button>
+        <br>
         
+    </div>
+
+    <input type="hidden" value="tags" name="module"><input type="hidden" value="tags" name="view"><input type="hidden" value="" name="task"><input type="hidden" value="0" name="boxchecked"><input type="hidden" value="0" name="page"><input type="hidden" value="L2FkbWluL3RhZ3MvdGFncy9hZGQ=" name="return">
+
+
+    
+</form>
+
