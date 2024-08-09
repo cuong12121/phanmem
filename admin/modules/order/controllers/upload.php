0dbcb240128a4e67183628756939e2cae4ef425a
cuong12121 <cuongdao12121@gmail.com>
2024-08-09 09:03:29 +0700

    Update upload.php

--- admin/modules/order/controllers/upload.php
+++ admin/modules/order/controllers/upload.php
@@ -1197,7 +1197,7 @@
 						    else{
 
 
-						    	$html .= '<a target="_blank" style="color: red;" href="'.basename($file_namesss).'">'.basename($file_namesss).'</a><br/>';
+						    	$html .= '<a target="_blank" style="color: green;" href="'.basename($file_namesss).'">'.basename($file_namesss).'</a><br/>';
 
 						       
 						    }
