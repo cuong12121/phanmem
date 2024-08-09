49e85a31fd0ae16bf2464734aa3baa0a369dbd7b
cuong12121 <cuongdao12121@gmail.com>
2024-08-09 11:35:55 +0700

    Update form.php

--- admin/modules/order/views/external/form.php
+++ admin/modules/order/views/external/form.php
@@ -1,142 +1,27 @@
-<?php
+<?php  
+    global $toolbar;
+    $toolbar->setTitle(FSText :: _('Từ khóa trèn trong mô tả sản phẩm') );
+    $toolbar->addButton('duplicate',FSText :: _('Duplicate'),'','duplicate.png');
+    $toolbar->addButton('save_all',FSText :: _('Save'),'','save.png'); 
+    $toolbar->addButton('add',FSText :: _('Add'),'','add.png'); 
+    $toolbar->addButton('edit',FSText :: _('Edit'),FSText :: _('You must select at least one record'),'edit.png'); 
+    $toolbar->addButton('remove',FSText :: _('Remove'),FSText :: _('You must select at least one record'),'remove.png'); 
+    $toolbar->addButton('published',FSText :: _('Published'),FSText :: _('You must select at least one record'),'published.png');
+    $toolbar->addButton('unpublished',FSText :: _('Unpublished'),FSText :: _('You must select at least one record'),'unpublished.png');
+    
+    //  FILTER
+    $filter_config  = array();
+    $fitler_config['search'] = 1; 
 
-$title = @$data ? FSText :: _('Edit'): FSText :: _('Add'); 
-
-
-global $toolbar;
-$toolbar->setTitle($title);
-$toolbar->addButton('save_add',FSText :: _('Save and new'),'','save_add.png');
-$toolbar->addButton('apply',FSText :: _('Apply'),'','apply.png'); 
-$toolbar->addButton('Save',FSText :: _('Save'),'','save.png'); 
-$toolbar->addButton('back',FSText :: _('Cancel'),'','back.png'); 
-
-
-// Tạo trang HTML và nhúng mã vạch vào
+    //  CONFIG  
+    $list_config = array();
+    $list_config[] = array('title'=>'Tên','field'=>'name','ordering'=> 1, 'type'=>'edit_text','col_width' => '20%','arr_params'=>array('size'=> 20));
+    $list_config[] = array('title'=>'Link','field'=>'link','ordering'=> 1, 'type'=>'edit_text','col_width' => '30%','arr_params'=>array('size'=> 50));
+    $list_config[] = array('title'=>'Ordering','field'=>'ordering','ordering'=> 1, 'type'=>'edit_text','arr_params'=>array('size'=>3));
+    $list_config[] = array('title'=>'Published','field'=>'published','ordering'=> 1, 'type'=>'published');
+//  $list_config[] = array('title'=>'Bôi đậm','field'=>'is_bold','ordering'=> 1, 'type'=>'change_status','arr_params'=>array('function'=>'bold'));
+    $list_config[] = array('title'=>'Edit','type'=>'edit');
+    $list_config[] = array('title'=>'Id','field'=>'id','ordering'=> 1, 'type'=>'text');
+    
+    TemplateHelper::genarate_form_liting($this, $this->module,$this -> view,$list,$fitler_config,$list_config,$sort_field,$sort_direct,$pagination);
 ?>
-<div class="form_head"><div id="wrap-toolbar" class="wrap-toolbar">  <div class="fl">                      <h1 class="page-header"> Thêm mới</h1><!--end: .page-header -->
-                   
-<!-- /.row -->    
-  </div>  <div class="fr"><a class="toolbar" onclick="javascript: submitbutton('save_add')" href="#"><span title="Save and new" style="background:url('https://dienmayai.com/admin/templates/default/images/toolbar/save_add.png') no-repeat"></span>Save and new</a><a class="toolbar" onclick="javascript: submitbutton('apply')" href="#"><span title="Apply" style="background:url('https://dienmayai.com/admin/templates/default/images/toolbar/apply.png') no-repeat"></span>Apply</a><a class="toolbar" onclick="javascript: submitbutton('Save')" href="#"><span title="Lưu" style="background:url('https://dienmayai.com/admin/templates/default/images/toolbar/save.png') no-repeat"></span>Lưu</a><a class="toolbar" onclick="javascript: submitbutton('back')" href="#"><span title="Thoát" style="background:url('https://dienmayai.com/admin/templates/default/images/toolbar/back.png') no-repeat"></span>Thoát</a>      <div class="clearfix"></div>  </div>  <div class="clearfix"></div></div><!--end: .wrap-toolbar--></div>
-
-<style type="text/css">
-    /* CSS Styling */
-    body {
-    font-family: sans-serif; /* Sử dụng phông chữ không chân */
-    }
-    form {
-    width: 1600px; /* Điều chỉnh độ rộng của form */
-    margin: 0 auto; /* Căn giữa form */
-    padding: 20px;
-    border: 1px solid #ccc;
-    height: 1000px;
-    }
-    label {
-    display: block;
-    margin-bottom: 5px;
-    }
-    input[type="text"] {
-    width: 100%;
-    padding: 8px;
-    margin-bottom: 10px;
-    box-sizing: border-box;
-    border: 1px solid #ccc;
-    }
-    table {
-    width: 100%;
-    border-collapse: collapse;
-    margin-top: 20px;
-    }
-    th, td {
-    border: 1px solid #ccc;
-    padding: 8px;
-    text-align: left;
-    }
-
-   
-    th {
-    background-color: #f0f0f0;
-    }
-    input[type="submit"] {
-    background-color: #4CAF50;
-    color: white;
-    padding: 10px 15px;
-    border: none;
-    cursor: pointer;
-    }
-    input[type="submit"]:hover {
-    background-color: #45a049;
-    }
-
-    
-
-
-    .right{
-        float: left;
-        width: 100%;
-    }
-</style>
-    
-    
-<form method="post" action="">
-    
-    <div class="right">
-        <label for="requester">Họ và tên người yêu cầu xuất:</label><br>
-        <input type="text" id="requester" name="requester" required><br><br>
-        <label for="storeName">Tên gian hàng:</label><br>
-        <input type="text" id="storeName" name="storeName" required><br><br>
-        <label for="deliveryPerson">Tên NVC giao:</label><br>
-        <input type="text" id="deliveryPerson" name="deliveryPerson" required><br><br>
-        <label for="department">Bộ phận:</label><br>
-        <input type="text" id="department" name="department" required><br><br>
-        <label for="storeCode">Mã gian hàng:</label><br>
-        <input type="text" id="storeCode" name="storeCode" required><br><br>
-        <label for="customerPhone">Số điện thoại khách hàng:</label><br>
-        <input type="text" id="customerPhone" name="customerPhone" required><br><br>
-
-        <label for="diachinhanhang">Địa chỉ nhận hàng:</label><br>
-        <input type="text" id="diachinhanhang" name="diachinhanhang" required><br><br>
-
-         <label for="tennguoinhan">Tên người nhận hàng:</label><br>
-        <input type="text" id="tennguoinhan" name="tennguoinhan" required><br><br>
-        <table>
-            <tr>
-                <th>TT</th>
-                <th style="width: 267px;">Tên sản phẩm</th>
-                <th>Mã đơn hàng</th>
-
-                <th style="width: 10px;">Số lượng</th>
-                <th>Phí vận chuyển</th>
-                
-                <th style="width:105px">Tổng số tiền người mua thanh toán</th>
-                
-                <th style="width:105px">Đơn giá</th>
-                <th>Thành tiền</th>
-                <th>Họ tên người thu tiền</th>
-                <th>Ghi chú (Mã vận đơn)</th>
-            </tr>
-            <tr>
-                <td>1</td>
-                <td><input type="text" id="productName1" name="productName1" required></td>
-                <td><input type="text" id="productCode1" name="productCode1" required></td>
-
-                <td><input type="text" id="soluong" name="soluong" required></td>
-                <td><input type="text" id="phivanchuyen" name="phivanchuyen" required></td>
-                <!-- <td><input type="text" id="phivanchuyennguoimuatra" name="phivanchuyennguoimuatra"></td> -->
-                <td><input type="text" id="tongsotiennguoimuathanhtoan" name="tongsotiennguoimuathanhtoan" required></td>
-               
-                <td><input type="text" id="dongia" name="dongia" required></td>
-
-
-                <td><input type="text" id="thanhtien" name="thanhtien" required></td>
-                <td><input type="text" id="hotenguoithutien" name="hotenguoithutien" required></td>
-                <td><input type="text" id="ghichu" name="ghichu" required></td>
-                
-            </tr>
-            
-        </table>
-        <br>
-        
-    </div>
-    
-</form>
-
