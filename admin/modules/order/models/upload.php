4a615fc20f9a67525693c18088e82f7f747cdade
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2024-08-25 16:22:55 +0700

    Update upload.php

--- admin/modules/order/models/upload.php
+++ admin/modules/order/models/upload.php
@@ -1771,8 +1771,8 @@
 
 			$url = 'http://test.dienmayai.com/';
 
-			$row['file_pdf'] = str_replace('/www/wwwroot/test.dienmayai.com/', 'files', $filepdf);
-			$row['file_xlsx'] =  str_replace('/www/wwwroot/test.dienmayai.com/', 'files', $file_xlsx) ;
+			$row['file_pdf'] = str_replace('/www/wwwroot/test.dienmayai.com/', 'files/', $filepdf);
+			$row['file_xlsx'] =  str_replace('/www/wwwroot/test.dienmayai.com/', 'files/', $file_xlsx) ;
 			$row['date'] = date('Y-m-d');
 			$row['user_id'] = $_SESSION['ad_userid'];
 			$shop_id = 286;
