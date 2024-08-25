b8fac784e8e483c62d378237572d749503b26803
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2024-08-25 17:08:38 +0700

    Update upload.php

--- admin/modules/order/controllers/upload.php
+++ admin/modules/order/controllers/upload.php
@@ -1294,7 +1294,7 @@
 		
 		if(empty($data_detail)){
 		   
-			return '<a style="color: red;" target="_blink">Lỗi </a>';
+			return '<a style="color: red;" target="_blink">Lỗi 14 </a>';
 		}else{
 			return '<a style="color: rgba(255, 153, 0, 0.79);" target="_blink" href="' . $link . '">'.basename($data-> file_xlsx).'</a>';
 		}
