253a45fc44317946e6c622c2ccf8a4ba0a128252
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2024-10-17 14:54:51 +0700

    Update upload.php

--- admin/modules/order/models/upload.php
+++ admin/modules/order/models/upload.php
@@ -2710,7 +2710,7 @@
 					$item_page_pdf-> content = str_replace('(**) 1','(**)1 ',$item_page_pdf-> content);
 					
 
-					if(!empty($this -> prefix.'filter2') && $_SESSION[$this -> prefix.'filter2'] == 1){
+					if(!empty($_SESSION[$this -> prefix.'filter2']) && $_SESSION[$this -> prefix.'filter2'] == 1){
 						//check xem file này có lỗi mã đầu nhảy xuống cuối ko.
 						$lzd_content_arr = explode('Lut Bu Chnh.',$item_page_pdf-> content);
 						if(!empty($lzd_content_arr[1])){
@@ -2730,7 +2730,7 @@
 					}
 
 					//// c067-S1-02-QSF-200g-tom-ha-tien fix lỗi hải sản
-					if(!empty($this -> prefix.'filter2') && $_SESSION[$this -> prefix.'filter0'] == 1){
+					if(!empty($_SESSION[$this -> prefix.'filter0']) && $_SESSION[$this -> prefix.'filter0'] == 1){
 
 						preg_match('/[A-Za-z0-9][A-Za-z0-9][A-Za-z0-9][A-Za-z0-9]+-[A-Za-z0-9][A-Za-z0-9]+-[A-Za-z0-9][A-Za-z0-9]+-[A-Za-z0-9][A-Za-z0-9]+-/', $item_page_pdf-> content, $b);
 
