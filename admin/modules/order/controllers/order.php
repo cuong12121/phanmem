20f533124904ece5c7c069d996a149aaa5bc1260
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2024-12-12 16:05:37 +0700

    Update order.php

--- admin/modules/order/controllers/order.php
+++ admin/modules/order/controllers/order.php
@@ -641,9 +641,9 @@
 			foreach ($data as $key => $value) {
 				$model = trim($value['model']);
 				$name = trim($value['name']);
-				dd($value['image']);
 				
-				$image =  str_replace('/www/wwwroot/dienmayai.com/', '', $this->convert_json_data($value['image'], $model)) ;
+
+				$image =  $value['image'];
 				$barcode = trim($value['model']);
 
 
