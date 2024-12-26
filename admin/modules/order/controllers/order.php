d728821889f7a04976c5bdab4a6f2185c0b0b775
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2024-12-26 17:19:17 +0700

    Update order.php

--- admin/modules/order/controllers/order.php
+++ admin/modules/order/controllers/order.php
@@ -112,6 +112,26 @@
 
 		function update_by_api_packed()
 		{
+			$config = require PATH_BASE.'/includes/configs.php';
+
+			$host = $config['dbHost'];
+			$db = $config['dbName'];
+			$user = $config['dbUser'];
+			$pass = $config['dbPass'];
+			$charset = 'utf8mb4';
+
+			$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+			$options = [
+			    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+			    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+			    PDO::ATTR_EMULATE_PREPARES   => false,
+			];
+
+			try {
+			    $pdo = new PDO($dsn, $user, $pass, $options);
+			} catch (\PDOException $e) {
+			    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+			}
 			
 	        $context = stream_context_create(array(
 	            'http' => array(
