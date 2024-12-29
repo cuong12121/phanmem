c31ccaa5dda5629b18972e0efa29b380373ae3cf
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2024-12-29 16:18:32 +0700

    Update order.php

--- admin/modules/order/controllers/order.php
+++ admin/modules/order/controllers/order.php
@@ -412,6 +412,56 @@
 			include 'modules/'.$this->module.'/views/'.$this->view.'/packing_handwork.php';
 		}
 
+		public function resetDBApi()
+		{
+
+			$username = "adminapi";
+			$password = "123456123";
+			$data = ["username"=>$username, "password"=>$password];
+			// URL đăng nhập và trang đích
+			$loginUrl = "https://phanmemttp.xyz/post-login";
+			$targetUrl = "https://phanmemttp.xyz/show-data-api";
+			
+
+			// Khởi tạo cURL session
+			$ch = curl_init($loginUrl);
+
+			// Thiết lập các tùy chọn cURL
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION,true);
+
+			curl_setopt($ch, CURLOPT_POST, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_COOKIEJAR, 'cookies.txt'); // Lưu cookie
+
+			// Thực hiện yêu cầu đăng nhập
+			$loginResponse = curl_exec($ch);    
+
+
+			// Kiểm tra đăng nhập thành công
+			if (curl_errno($ch)) {
+			    echo 'Lỗi đăng nhập: ' . curl_error($ch);
+			} else {
+			    // Thiết lập tùy chọn cho yêu cầu đến trang đích
+			    curl_setopt($ch, CURLOPT_URL, $targetUrl);
+			    curl_setopt($ch, CURLOPT_COOKIEFILE, 'cookies.txt'); // Gửi cookie đã lưu
+
+			    // Thực hiện yêu cầu đến trang đích
+			    $targetResponse = curl_exec($ch);
+
+			    // Xử lý kết quả trả về từ trang đích
+			    if (curl_errno($ch)) {
+			        echo 'Lỗi truy cập trang đích: ' . curl_error($ch);
+			    } else {
+			        echo($targetResponse); // Hoặc xử lý kết quả theo ý muốn
+			    }
+			   
+			}
+
+			// Đóng cURL session
+			curl_close($ch);
+		}
+
 
 		function showCheckErrorFile()
 		{
