db530cc55b1461b5e084a6b9ddbd4f3e22e24d02
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2025-01-13 20:38:56 +0700

    Update order.php

--- admin/modules/order/controllers/order.php
+++ admin/modules/order/controllers/order.php
@@ -1075,6 +1075,28 @@
 
 		function add_box_Order()
 		{
+			// phần kết nối DB
+			$config = require PATH_BASE.'/includes/configs.php';
+
+			$host = $config['dbHost'];
+			$db = $config['dbName'];
+			$user = $config['dbUser'];
+			$pass = $config['dbPass'];
+			$charset = 'utf8mb4';
+
+			$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+			$options = [
+			    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+			    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+			    PDO::ATTR_EMULATE_PREPARES   => false,
+			];
+
+			try {
+			    $pdo = new PDO($dsn, $user, $pass, $options);
+			} catch (\PDOException $e) {
+			    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+			}
+			// end kết nối DB
 
 			date_default_timezone_set('Asia/Ho_Chi_Minh');
 
