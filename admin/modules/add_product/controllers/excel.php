ae491e16d5784a325b2734c69ecfde040f64dd95
cuong12121 <cuongdao12121@gmail.com>
2025-03-28 10:41:21 +0700

    Update excel.php

--- admin/modules/add_product/controllers/excel.php
+++ admin/modules/add_product/controllers/excel.php
@@ -313,14 +313,18 @@
 					
 					$check = $model->get_record('code = "'.$code.'"','fs_products','code,id');
 
-					// echo "<pre>";
-
-					// var_dump($check);
-
-					// echo "</pre>";
-
-					// die;
 					if(!empty($check)){
+						$row['price'] ===0?unset($row['price']):$row['price'];
+
+						$row['price_pack'] ===0?unset($row['price_pack']):$row['price_pack'];
+
+						$row['price_wholesale'] ===0?unset($row['price_wholesale']):$row['price_wholesale'];
+
+						$row['price_old'] ===0?unset($row['price_old']):$row['price_old'];
+
+						$row['price_min'] ===0?unset($row['price_min']):$row['price_min'];
+
+
 
 						var_dump($row);
 
