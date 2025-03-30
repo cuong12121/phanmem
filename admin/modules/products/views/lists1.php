dbe1c47cd67b4e756f2217662a6565f02d5ba4fe
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2025-03-30 20:02:49 +0700

    Update lists1.php

--- admin/modules/products/views/lists1.php
+++ admin/modules/products/views/lists1.php
@@ -176,6 +176,65 @@
   <script type="text/javascript" src="https://dienmayai.com/admin/templates/default/js/jquery-confirm.min.js"></script>
         <script type="text/javascript" src="https://dienmayai.com/admin/templates/default/js/select2.min.js"></script>
 <script type="text/javascript" src="https://dienmayai.com/admin/templates/default/js/helper.js?t=1743337731"></script>
+     <!-- Bootstrap Core JavaScript -->
+        <script src="https://dienmayai.com/admin/templates/default/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+
+        <!-- Metis Menu Plugin JavaScript -->
+        <script src="https://dienmayai.com/admin/templates/default/bower_components/metisMenu/dist/metisMenu.min.js"></script>
+
+        <!-- Morris Charts JavaScript -->
+        <script src="https://dienmayai.com/admin/templates/default/bower_components/raphael/raphael-min.js"></script>
+
+        <!-- DataTables JavaScript -->
+        <script src="https://dienmayai.com/admin/templates/default/bower_components/datatables/media/js/jquery.dataTables.min.js"></script>
+        <script src="https://dienmayai.com/admin/templates/default/bower_components/datatables-plugins/integration/bootstrap/3/dataTables.bootstrap.min.js"></script>
+        <script src="https://dienmayai.com/admin/templates/default/bower_components/datatables-responsive/js/dataTables.responsive.js"></script>
+        <script>
+            $(document).ready(function() {
+                $('#dataTables-example1').DataTable({
+                    responsive: true,
+                    "language": {
+                        "lengthMenu": "Hiển thị _MENU_ trên mỗi trang",
+                        "zeroRecords": "Không tìm thấy gì - xin lỗi",
+                        "info": "Đang ở trang _PAGE_ của _PAGES_",
+                        "infoEmpty": "Không có dữ liệu có sẵn",
+                        "infoFiltered": "(lọc từ tổng số hồ sơ _MAX_)",
+                        "search": "Tìm kiếm nhanh:",
+                        paginate: {
+                            first:    '«',
+                            previous: '‹',
+                            next:     '›',
+                            last:     '»'
+                        },
+                    },
+                    select: {
+                        style: 'multi'
+                    },
+                    "lengthMenu": [ 10 ,20, 30, 40 , 50],
+                    "columnDefs": [
+                    { "orderable": false, "targets": 1 }
+                    ]
+                });
+
+            });
+        </script>
+        <!-- Custom Theme JavaScript -->
+        <script src="https://dienmayai.com/admin/templates/default/dist/js/sb-admin-2.js"></script>
+        <script src="https://dienmayai.com/admin/templates/default/dist/js/jquery.cookie.js"></script>
+        <!-- Custom select chosen.jquery.js -->
+        <script src="https://dienmayai.com/admin/templates/default/js/chosen.jquery.js" type="text/javascript"></script>
+        <script type="text/javascript">
+            var config = {
+              '.chosen-select'           : {no_results_text: "Không tìm thấy "},
+              '.chosen-select-deselect'  : {allow_single_deselect:true},
+              '.chosen-select-no-single' : {disable_search_threshold:10},
+              '.chosen-select-no-results': {no_results_text:"Không tìm thấy "},
+              '.chosen-select-width'     : {width:"95%"}
+          }
+          for (var selector in config) {
+              $(selector).chosen(config[selector]);
+          }
+      </script>
 
 </body>
 </html>
