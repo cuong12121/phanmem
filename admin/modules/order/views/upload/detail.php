2382e21dd37ad88041f6989565fbb8a2356f8937
cuong12121 <cuongdao12121@gmail.com>
2025-04-08 16:21:41 +0700

    Update detail.php

--- admin/modules/order/views/upload/detail.php
+++ admin/modules/order/views/upload/detail.php
@@ -66,7 +66,7 @@
             }
             else{
 
-                var file = this.files[0].files[0];
+                var file = this.files[0].files;
 
                 var formData = new FormData();
                 formData.append('excel', file);
