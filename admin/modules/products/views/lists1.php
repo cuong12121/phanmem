79cc53cd8e044f07ec4737f5a466391aab463d37
cuong12121 <cuongdao12121@gmail.com>
2025-04-23 11:34:10 +0700

    Update lists1.php

--- admin/modules/products/views/lists1.php
+++ admin/modules/products/views/lists1.php
@@ -386,7 +386,7 @@
                 <td>
                     <div class="wrap_list_pr"><a title="Sửa" href="/admin/product/edit/<?= $value->id ?>"><img border="0" alt="Sửa" src="/admin/templates/default/images/edit.png"></a></div>
                 </td>
-                <td><a title="Xóa" href="/index.php?module=products&view=products&task=delete_product&product_id=<?= $value->id ?>" class="link_delete"><img border="0" alt="xóa" src="/admin/templates/default/images/toolbar/remove.png"></a></td>
+                <td><a title="Xóa" href="/admin/index.php?module=products&view=products&task=delete_product&product_id=<?= $value->id ?>" class="link_delete"><img border="0" alt="xóa" src="/admin/templates/default/images/toolbar/remove.png"></a></td>
             </tr>
             
             <?php
