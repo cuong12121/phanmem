5ded1c5e7555a5e98fcd69f8a6c1383644c87045
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2025-05-10 08:53:40 +0700

    Update products.php

--- admin/modules/products/controllers/products.php
+++ admin/modules/products/controllers/products.php
@@ -1395,14 +1395,7 @@ class ProductsControllersProducts  extends Controllers
 
 			$in_clause = implode(',', $escaped_codes);
 
-			echo "<pre>";
-
-			var_dump($in_clause);
-
-			echo "</pre>";
-
-			die;
-
+			
 			// Kết nối MySQLi
 			$mysqli = new mysqli("localhost", "sql_dienmay_bak", "bfsHT6wL4GBJnAYA", "sql_dienmay_bak");
 			if ($mysqli->connect_error) {
