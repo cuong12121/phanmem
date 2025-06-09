bdd0036451cbfe61a7216d650519cf12eafa0b40
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2025-06-10 02:15:25 +0700

    Update history.php

--- admin/modules/print_history/controllers/history.php
+++ admin/modules/print_history/controllers/history.php
@@ -308,6 +308,12 @@
 			$model->calculateCumulativeQuantities($data_result);
 
 			$data_result = $model->show_list_array_run($data_result);
+
+			echo "<pre>";
+			print_r($data_result);
+			echo "<pre>";
+
+			die;
 			
 			$pdf = new Fpdi();
 
