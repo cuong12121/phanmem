414b5a9b1afd1d6e5fda8e64312d9beec6c61564
cuong12121 <cuongdao12121@gmail.com>
2025-06-11 14:30:59 +0700

    Update history.php

--- admin/modules/print_history/controllers/history.php
+++ admin/modules/print_history/controllers/history.php
@@ -378,56 +378,51 @@
 
 			    $data = $this->return_product_sku_quantity_to_text($text);
 
-			    echo "<pre>";
 
-			    print_r($data);
-			    echo "</pre>";
 
-			    die;
+			    if(!empty($data) && count($data)>0){
 
-			    // if(!empty($data) && count($data)>0)
+				    for ($i = 0; $i < count($data); $i++) {
 
-				//     for ($i = 0; $i < count($data); $i++) {
-
-				//     	$check_sl = [];
+				    	$check_sl = [];
 				    	
-				//         $skuFull = $data[$i]['sku'];
-				//         $skuShort = substr($skuFull, 0, 7); // Lấy 4 ký tự đầu của SKU
+				        $skuFull = $data[$i]['sku'];
+				        $skuShort = substr($skuFull, 0, 7); // Lấy 4 ký tự đầu của SKU
 
-				//         $sku_full_check = substr(trim($skuFull), 0, 10); // Lấy 10 ký tự đầu của SKU
+				        $sku_full_check = substr(trim($skuFull), 0, 10); // Lấy 10 ký tự đầu của SKU
 
-				//         $check_combo = $this->combo_Return_code($sku_full_check);
+				        $check_combo = $this->combo_Return_code($sku_full_check);
 
-				//         $quantity_get = $data[$i]['quantity'];
+				        $quantity_get = $data[$i]['quantity'];
 
-				//         if(!empty($check_combo)){
+				        if(!empty($check_combo)){
 				        	
-				//         	$show_more = $check_combo;
+				        	$show_more = $check_combo;
 
-				//         	$ar_sku_show[$index][] =  $show_more;
+				        	$ar_sku_show[$index][] =  $show_more;
 
-				//         }
+				        }
 				        
-				//         $results[] = [
-				//             'sku' => $skuShort,
-				//             'quantity' => $quantity_get,
-				//             'sku_full' => $skuFull,
-				//             'sku_full_check' => $sku_full_check,
-				//             'count_show_more'=> !empty($check_combo)?count($show_more):0,
+				        $results[] = [
+				            'sku' => $skuShort,
+				            'quantity' => $quantity_get,
+				            'sku_full' => $skuFull,
+				            'sku_full_check' => $sku_full_check,
+				            'count_show_more'=> !empty($check_combo)?count($show_more):0,
 				           
-				//         ];
-				//     }
-				// }    
-			    // array_push($data_result, array_reverse($results));
+				        ];
+				    }
+				}    
+			    array_push($data_result, array_reverse($results));
 			 
 			}
-			// echo "<pre>";
+			echo "<pre>";
 
-			// print_r($data_result);
+			print_r($data_result);
 
-			// echo "<pre>";
+			echo "<pre>";
 
-			// die;
+			die;
 
 			foreach ($ar_sku_show as $key => $group) {
 			    // Nếu là mảng chứa nhiều mảng con, thì gộp lại
