d0cc4cc832c860135eda48bccdb886143fa7645c
cuong12121 <cuongdao12121@gmail.com>
2025-06-11 08:31:49 +0700

    Update history.php

--- admin/modules/print_history/controllers/history.php
+++ admin/modules/print_history/controllers/history.php
@@ -234,7 +234,9 @@
 		{
 			$model  = $this -> model;
 
-			$filePath = 'https://dienmayai.com/files/orders/2025/06/08/hn86nnt3_1749400132_cv.pdf';
+			$filePath = $_ENV['file'];
+
+			// $filePath = 'https://dienmayai.com/files/orders/2025/06/08/hn86nnt3_1749400132_cv.pdf';
 
 			// Load PDF file
 			$parser = new Parser();
