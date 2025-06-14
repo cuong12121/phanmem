2a214dcb5b655be518b736dbe0125aca05ab0dd0
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2025-06-14 11:28:30 +0700

    Update history.php

--- admin/modules/print_history/controllers/history.php
+++ admin/modules/print_history/controllers/history.php
@@ -296,6 +296,10 @@
 
 			$data_ex = $this->data_excel($url_ex);
 
+			echo"<pre>";
+			print_r($data_ex);
+			echo"</pre>";
+			die;
 
 			$norm1 = normalize_array($data_pdfs);
 			$norm2 = normalize_array($data_ex);
