591755f6e3b6f60c995b488a39b9365e7f4c6bcc
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2025-06-14 10:41:52 +0700

    update

--- admin/modules/print_history/controllers/history.php
+++ admin/modules/print_history/controllers/history.php
@@ -746,7 +746,7 @@
 		{
 			// $files = 'ex2.xlsx';
             // $file_path = PATH_BASE.'files/'.$files;
-            require_once("PHPExcel-1.8/Classes/PHPExcel.php");
+            require_once("/libraries/PHPExcel-1.8/Classes/PHPExcel.php");
             $objReader = PHPExcel_IOFactory::createReaderForFile($file_path);
             // $data = new PHPExcel_IOFactory();
             // $data->setOutputEncoding('UTF-8');
