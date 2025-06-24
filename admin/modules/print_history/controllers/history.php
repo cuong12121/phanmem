7f1c21e8bb709c0ec69d8a22eb7f01a0bdce7400
cuong12121 <cuongdao12121@gmail.com>
2025-06-24 14:20:51 +0700

    Update history.php

--- admin/modules/print_history/controllers/history.php
+++ admin/modules/print_history/controllers/history.php
@@ -620,7 +620,7 @@
 			$houseid = 6;
 
 
-			$urls = ['https://dienmayai.com/files/orders/2025/06/24/time_18_warehouse_6_platform_id_2_date_1750743717.pdf'];
+			$urls = ['https://dienmayai.com/files/orders/2025/06/23/time_13_warehouse_6_platform_id_2_date_1750723911.pdf'];
 
 
 			$filename = $model->downloadMultipleFiles($urls);
