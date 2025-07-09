a56cfaa8a34589db62b1958399ec5cbbccf6cf57
cuong12121 <cuongdao12121@gmail.com>
2025-07-09 10:52:22 +0700

    Update history.php

--- admin/modules/print_history/controllers/history.php
+++ admin/modules/print_history/controllers/history.php
@@ -787,8 +787,11 @@
 			    		//phần ghi mã sản phẩm khi có số sản phẩm lớn hơn 2
 			    		if(count($data_all)>1){
 			    			if(!empty($z)){
-			    				$i = $z+1;
+			    				$kk = $z+1;
 
+			    			}
+			    			else{
+			    				$kk = $i;
 			    			}
 			    			
 
@@ -799,8 +802,8 @@
 				    			$pdf->SetFont('Arial', 'B', 14);
 						    	$pdf->SetTextColor(0, 0, 0); // Màu đen
 
-						        $pdf->SetXY(105, $k[$i]);
-						        $write_show_more_pd = $data_result[$index_data][$i]['sku'].':'.$data_result[$index_data][$i]['quantity'];
+						        $pdf->SetXY(105, $k[$kk]);
+						        $write_show_more_pd = $data_all[$i]['sku'].':'.$data_result[$index_data][$i]['quantity'];
 
 						        $pdf->Write(10, $write_show_more_pd);
 
