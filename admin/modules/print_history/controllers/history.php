c0c90eb3c403b8c3a48469eb38f3ea714342a653
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2025-07-10 16:25:11 +0700

    Update history.php

--- admin/modules/print_history/controllers/history.php
+++ admin/modules/print_history/controllers/history.php
@@ -622,12 +622,20 @@
 
 			// Thêm trường sku_add
 			foreach ($data as &$group) {
+				$total_combo_count = 0;
 			    foreach ($group as &$item) {
+
 			    	$array_data = $this->combo_Return_code($item->sku_full_check);
 			    	
 
 			        $item->combo =  !empty($array_data)?$array_data['list']:'';
 			        $item->product_combo_code =  !empty($array_data)?$array_data['product_code']:'';
+			        if (is_array($item->combo)) {
+				        $total_combo_count += count($item->combo);
+				    } else {
+				        $total_combo_count += 1;
+				    }
+				    $item->count_combo = $total_combo_count;
 			    }
 			}
 			echo "<pre>";
