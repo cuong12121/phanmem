9ec361405d02759ab5c64d5cacaafe9b082742c7
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2025-07-10 16:42:56 +0700

    Update history.php

--- admin/modules/print_history/controllers/history.php
+++ admin/modules/print_history/controllers/history.php
@@ -622,9 +622,18 @@
 
 			// Thêm trường sku_add
 			foreach ($data as &$group) {
+			    foreach ($group as &$item) {
+			    	$array_data = $this->combo_Return_code($item->sku_full_check);
+			    	
+
+			        $item->combo =  !empty($array_data)?$array_data['list']:'';
+			        $item->product_combo_code =  !empty($array_data)?$array_data['product_code']:'';
+			    }
+			}
+
+			foreach ($data as &$group) {
 				$result = [];
 			    foreach ($group as &$item) {
-
 			    	if (is_array($item->combo) && count($item->combo) > 0) {
 				        foreach ($item->combo as $combo_item) {
 				            $result[] = $combo_item;
@@ -632,13 +641,12 @@
 				    } else {
 				        $result[] = $item->sku . ':' . $item->quantity;
 				    }
-			    }
+			    }	
 			    echo "<pre>";
-				print_r($result);
-				echo "</pre>";
-			}
-			
+			    	print_r($result);
+			    echo "</pre>";
 
+			}			
 		}
 
 		function clone_function()
@@ -683,21 +691,12 @@
 				
 
 				foreach ($data_result as &$group) {
-					$total_combo_count = 0;
-
 				    foreach ($group as &$item) {
 				    	$array_data = $this->combo_Return_code($item['sku_full_check']);
 				    	
 
 				        $item['combo'] = !empty($array_data) ? $array_data['list'] : '';
 				        $item['product_combo_code'] = !empty($array_data) ? $array_data['product_code'] : '';
-				        if (is_array($item->combo)) {
-					        $total_combo_count += count($item['combo']);
-					    } else {
-					        $total_combo_count += 1;
-					    }
-					    $item['count_combo'] = $total_combo_count;
-					   
 				        // $item['count_show_more'] = !empty($array_data['list']) ? count($array_data['list']) : 0;
 				    }
 				}
