18eb152c9c0eab96e5d95796fe4cab7d4cf5e4b6
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2025-07-28 10:25:56 +0700

    Update history.php

--- admin/modules/print_history/controllers/history.php
+++ admin/modules/print_history/controllers/history.php
@@ -917,12 +917,14 @@
 				    	if(count($result_print[$index_data])>1){
 
 				    		foreach ($result_print[$index_data] as $keysss => $value) {
-
+				    			echo $k[$keysss];
 				    			
 						        $pdf->SetXY(105, $k[$keysss]);
 				    			$writes = $value;
 				    			$pdf->Write(10, $writes);
 				    		}	
+
+				    		die;
 
 				    	}
 
