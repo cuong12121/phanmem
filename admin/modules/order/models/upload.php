a7b1bc637cf2a64259b60514398da1d1a7f25e4f
cuong12121 <cuongdao12121@gmail.com>
2025-08-05 08:50:16 +0700

    Update upload.php

--- admin/modules/order/models/upload.php
+++ admin/modules/order/models/upload.php
@@ -1637,13 +1637,6 @@
 
 			$data_id_user = $_SESSION['ad_userid'];
 
-			if($data_id_user==9){
-				
-				echo "ok";
-
-				die;
-			}
-
 			$redis = $this->connect_redis();
 
 			$data_tracking = [];
@@ -1850,7 +1843,12 @@
 
 				$check_pdf_ar = explode(',', $file_pdf_converts);
 
+				if($data_id_user==9){
+				
+					var_dump($check_pdf_ar);
 
+					die;
+				}
 
 				if(is_array($check_pdf_ar)&& count($check_pdf_ar)>1){
 					// phần này chạy lưu lên drive, sửa lại
