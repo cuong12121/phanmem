8da0098be914ff2ece1c0ae2ae54d8fe30279b98
cuong12121 <cuongdao12121@gmail.com>
2025-08-15 15:23:15 +0700

    Update upload.php

--- admin/modules/order/controllers/upload.php
+++ admin/modules/order/controllers/upload.php
@@ -1162,6 +1162,7 @@
 		{
 			global $db;
 			$model = $this->model;
+			$house_id =18;
 
 			for ($i=1; $i < 3; $i++) { 
 				$query =  "SELECT id FROM fs_order_uploads AS a WHERE 1=1 AND warehouse_id = $i AND house_id = 13 AND platform_id = 9 AND date ='".date('Y-m-d')."' ORDER BY created_time DESC , id DESC";
@@ -1195,7 +1196,7 @@
 
             	}
 
-            	$house_id =18;
+
  
             	$file_pdf_print = $model->merge_file($list_file_pdf, $house_id);
 
