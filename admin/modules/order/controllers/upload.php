c14abfcae574e41792e5a5e1113dda4527c2c5f2
cuong12121 <cuongdao12121@gmail.com>
2025-08-22 14:22:42 +0700

    Update upload.php

--- admin/modules/order/controllers/upload.php
+++ admin/modules/order/controllers/upload.php
@@ -1162,7 +1162,24 @@
 		{
 			global $db;
 			$model = $this->model;
-			$house_id =18;
+			$H = date('G');
+
+			if ($H < 8) {
+			    $house_id = 13;
+			}
+			elseif ($H >= 12 && $H < 13) {
+
+			    $house_id = 18;
+			}
+			elseif ($H >= 14 && $H < 15) {
+				
+			    $house_id = 15;
+			} 
+			else {
+			    $house_id = 20;
+			} 
+
+			
 
 			for ($i=1; $i < 3; $i++) { 
 				$query =  "SELECT id FROM fs_order_uploads AS a WHERE 1=1 AND warehouse_id = $i AND house_id = $house_id AND platform_id = 9 AND date ='".date('Y-m-d')."' ORDER BY created_time DESC , id DESC";
