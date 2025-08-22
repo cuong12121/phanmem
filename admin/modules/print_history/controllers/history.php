055c5e8a781afc3f7aca8da4fa0d9c09fe1e00d0
cuong12121 <cuongdao12121@gmail.com>
2025-08-22 11:55:00 +0700

    Update history.php

--- admin/modules/print_history/controllers/history.php
+++ admin/modules/print_history/controllers/history.php
@@ -1317,8 +1317,15 @@
 
 	function view_pdf_dem($controle,$id){
 		$model = $controle -> model;
-		$data = $model->get_record('id = ' .$id,'fs_order_uploads_history_prints','id,file_pdf_dem');
-		$link = URL_ROOT.$data-> file_pdf_dem;
+		$data = $model->get_record('id = ' .$id,'fs_order_uploads_history_prints','id,file_pdf_dem, platform_id');
+
+		if($data->platform_id==9){
+			$url = 'http://drive.phanmemttp.xyz/';
+		}
+		else{
+			$url = URL_ROOT;
+		}
+		$link = $url.$data-> file_pdf_dem;
 
 		$check = !empty($data-> file_pdf_dem)?'<a target="_blink" href="' . $link . '">Xem file</a>':'';
 
