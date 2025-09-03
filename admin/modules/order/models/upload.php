6bcec05759f231e9464ec2075196cec19d0c26c3
cuong12121 <cuongdao12121@gmail.com>
2025-09-03 14:21:09 +0700

    Update upload.php

--- admin/modules/order/models/upload.php
+++ admin/modules/order/models/upload.php
@@ -2874,7 +2874,7 @@
 
 
 				// test phần kiểm tra in với userid là admin 
-				// $userid = $_SESSION['userid'];
+				$userid =  $_SESSION['ad_userid'];
 
 				// var_dump($userid);
 
@@ -2890,11 +2890,11 @@
 				// $str_ids = implode(',',$ids);
 				$list = $this->get_records('id IN ('.$str_ids.')','fs_order_uploads');
 
-				// if($userid ==9){
+				if($userid ==9){
 
-				// 	echo '<pre>'; var_dump($list); echo'</pre>';
-				// 	die;
-				// }	
+					echo '<pre>'; var_dump($list); echo'</pre>';
+					die;
+				}	
 				
 				//kiểm tra các file pdf với excel có tồn tại hay không
 				foreach ($list as $item){
