96ff80c0c0a02c949d631ef212be37cf22daca86
cuong12121 <cuongdao12121@gmail.com>
2025-09-19 09:39:44 +0700

    Update history.php

--- admin/modules/print_history/controllers/history.php
+++ admin/modules/print_history/controllers/history.php
@@ -513,80 +513,72 @@
 
 				for ($pageNo = 1; $pageNo <= $pageCount; $pageNo++) {
 
-					// echo $pageNo;
-			        // $tplId = $pdf->importPage($i);
-			        // $size = $pdf->getTemplateSize($tplId);
+					
+			        $tplId = $pdf->importPage($i);
+			        $size = $pdf->getTemplateSize($tplId);
 
-			        // $width = $size['width'];
-			        // $height = $size['height'];
-			        // $new_height = $height + $extra_height;
+			        $width = $size['width'];
+			        $height = $size['height'];
+			        $new_height = $height + $extra_height;
 
 			        $index_data = $pageNo - 1;
 				    $data_all = $data_result[$index_data];
 
-				    echo "<pre>";
+			        // Tạo trang mới với chiều cao tăng thêm
+			        $pdf->AddPage('P', [$width, $new_height]);
 
-				    print_r($data_all);
+			        // Vẽ template cũ dịch lên trên
+			        $pdf->useTemplate($tplId, 0, 0, $width, $height);
 
-				    echo "</pre>";
+			        // Set font
+			        $pdf->SetFont('Arial', '', 10);
 
-				    
-
-			        // // Tạo trang mới với chiều cao tăng thêm
-			        // $pdf->AddPage('P', [$width, $new_height]);
-
-			        // // Vẽ template cũ dịch lên trên
-			        // $pdf->useTemplate($tplId, 0, 0, $width, $height);
-
-			        // // Set font
-			        // $pdf->SetFont('Arial', '', 10);
-
-			        // // Vẽ text trong vùng extra_height (tức phần trắng mới)
-			        // $page_num_text = $i . "/" . $pageCount;
+			        // Vẽ text trong vùng extra_height (tức phần trắng mới)
+			        $page_num_text = $pageNo . "/" . $pageCount;
 
 			   
-			        // if(!empty($data_all)&& count($data_all)>0){
-				    //     for ($i = 0; $i < count($data_all); $i++) {
+			        if(!empty($data_all)&& count($data_all)>0){
+				        for ($z = 0; $z < count($data_all); $z++) {
 
-					//     	//phần ghi mã sản phẩm khi có combo hoặc có sản phẩm nhiều hơn 2
-					//     	// if(count($result_print[$index_data])>1){
+					    	//phần ghi mã sản phẩm khi có combo hoặc có sản phẩm nhiều hơn 2
+					    	// if(count($result_print[$index_data])>1){
 
-					//     	// 	foreach ($result_print[$index_data] as $keysss => $value) {
-					//     	// 		$dong = $k[$keysss];
-					//     	// 		$dong += 5;
-					// 		//         $pdf->SetXY(105, $dong);
-					//     	// 		$writes = $value;
-					//     	// 		$pdf->Write(10, $writes);
-					//     	// 	}	
+					    	// 	foreach ($result_print[$index_data] as $keysss => $value) {
+					    	// 		$dong = $k[$keysss];
+					    	// 		$dong += 5;
+							//         $pdf->SetXY(105, $dong);
+					    	// 		$writes = $value;
+					    	// 		$pdf->Write(10, $writes);
+					    	// 	}	
 
 					    		
-					//     	// }
+					    	// }
 
 
-					//     	// $pdf->SetFont('Arial', 'B', 14);
-					//     	// $pdf->SetTextColor(0, 0, 0); // Màu đen
+					    	// $pdf->SetFont('Arial', 'B', 14);
+					    	// $pdf->SetTextColor(0, 0, 0); // Màu đen
 
-					//         $pdf->SetXY(10, $height+$i*8);
-					//         $write = $data_result[$index_data][$i]['parent_index'] . '--' .
-					//                  $data_result[$index_data][$i]['show_list'] . '==>' .
-					//                  $data_result[$index_data][$i]['all'] . '--' .
-					//                  $data_result[$index_data][$i]['all_to_sku'];
-					//         $pdf->Write(10, $write);
+					        $pdf->SetXY(10, $height+$z*8);
+					        $write = $data_result[$index_data][$z]['parent_index'] . '--' .
+					                 $data_result[$index_data][$z]['show_list'] . '==>' .
+					                 $data_result[$index_data][$z]['all'] . '--' .
+					                 $data_result[$index_data][$z]['all_to_sku'];
+					        $pdf->Write(10, $write);
 
-					//         // if(!empty($data_result[$index_data][$i]['combo'] &&count($data_result[$index_data][$i]['combo'])>0)){
-					//         // 	$in_cb = 'CB:'.$data_result[$index_data][$i]['product_combo_code'].':'.$data_result[$index_data][$i]['all_to_sku'];
-					//         // 	$y_in = $i+1+intval(count($data_all));
-					//         // 	$pdf->SetXY(105, $y[$y_in]);
-					//         // 	$pdf->Write(10, $in_cb);
-					//         // }
-					//     }
-					// }    
-			        // $pdf->SetXY(10, $height + 16); 
-			        // $pdf->Write(5, $page_num_text);
+					        // if(!empty($data_result[$index_data][$i]['combo'] &&count($data_result[$index_data][$i]['combo'])>0)){
+					        // 	$in_cb = 'CB:'.$data_result[$index_data][$i]['product_combo_code'].':'.$data_result[$index_data][$i]['all_to_sku'];
+					        // 	$y_in = $i+1+intval(count($data_all));
+					        // 	$pdf->SetXY(105, $y[$y_in]);
+					        // 	$pdf->Write(10, $in_cb);
+					        // }
+					    }
+					}    
+			        $pdf->SetXY(10, $height + 16); 
+			        $pdf->Write(5, $page_num_text);
 			    }
-			    die;
 
-			    // $pdf->Output('F', $output_pdf);
+
+			    $pdf->Output('F', $output_pdf);
 
 		    
 			}
