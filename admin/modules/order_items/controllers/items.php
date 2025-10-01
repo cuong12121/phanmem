13f5c60354982bf42f1b2e2178a30f2ed80f7b92
cuong12121 <cuongdao12121@gmail.com>
2025-10-01 16:16:13 +0700

    Update items.php

--- admin/modules/order_items/controllers/items.php
+++ admin/modules/order_items/controllers/items.php
@@ -1003,9 +1003,15 @@
 		function excel_tong_ngay(){
 
 			// xóa các file cũ trong thư mục cho nhẹ server
+			
+
 			$path_remove_file = PATH_ADMINISTRATOR.DS.'export'.DS.'excel'.DS.'order_item'.DS;
-			array_map('unlink', array_filter(
-       		(array) array_merge(glob($path_remove_file."*"))));
+
+			if (is_file($path_remove_file) && file_exists($path_remove_file)) {
+			    array_map('unlink', array_filter(
+       			(array) array_merge(glob($path_remove_file."*"))));
+			}
+			
 
 			FSFactory::include_class('excel','excel');
 			$model  = $this -> model;
