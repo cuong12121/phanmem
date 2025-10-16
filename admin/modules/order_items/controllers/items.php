75fa70034d89671869808c1c0a9206b00f03d664
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2025-10-16 18:02:04 +0700

    Update items.php

--- admin/modules/order_items/controllers/items.php
+++ admin/modules/order_items/controllers/items.php
@@ -163,7 +163,7 @@
 		{
 			$redis = new Redis();
 			$redis->connect('127.0.0.1', 6379); // IP & Port Redis server
-			$key = "list_xuat_kho";
+			$key = "complete_box";
 			$cache_data = $redis->get($key);
 
 			$list = json_decode($cache_data);
