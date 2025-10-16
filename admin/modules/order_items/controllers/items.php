f4aeae4f3a04bb6acb6ebc0fb78aa841f41443ca
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2025-10-16 16:11:23 +0700

    Update items.php

--- admin/modules/order_items/controllers/items.php
+++ admin/modules/order_items/controllers/items.php
@@ -114,9 +114,16 @@
 			$redis = new Redis();
 			$redis->pconnect('127.0.0.1', 6379); // IP & Port Redis server
 			$key = "list_xuat_kho";
+			if($_SESSION['ad_userid']==9){
+				$cache_data = $redis->get($key);
 
-			
-			$list = $this -> model->get_data();
+				$list = json_decode($cache_data);
+			}else{
+				$list = $this -> model->get_data();
+			}	
+
+
+
 			
 			$pagination = $model->getPagination();
 
