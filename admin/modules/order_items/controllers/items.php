e48ce7746b715e1974ebbb4a8a9185e384060c99
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2025-10-17 06:28:48 +0700

    Update items.php

--- admin/modules/order_items/controllers/items.php
+++ admin/modules/order_items/controllers/items.php
@@ -106,8 +106,21 @@
 			static $list = null;  //biến tĩnh dùng làm cache
 
 		    if ($list === null) {
-		        
-		       	$list = $this -> model->get_data();
+
+		    	// Kết nối Redis
+				$redis = new Redis();
+				$redis->pconnect('127.0.0.1', 6379); // IP & Port Redis server
+				$key = "list_xuat_kho";
+				if($_SESSION['ad_userid']==9){
+					$cache_data = $redis->get($key);
+
+					$list = json_decode($cache_data);
+				}
+				else{
+					$list = $this -> model->get_data();
+				}	
+		    	
+				
 		    }
 			
 
