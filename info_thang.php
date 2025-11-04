0b82563ac22a3bb431621343bfc430062c8d4de0
cuong12121 <cuongdao12121@gmail.com>
2025-11-04 09:19:40 +0700

    Update info_thang.php

--- info_thang.php
+++ info_thang.php
@@ -1,4 +1,18 @@
 <?php
-echo phpinfo();
+$redis = new Redis();
+$redis->connect('127.0.0.1', 6379); // IP & Port Redis server
+$key = "list_xuat_kho_t8_t10";
+
+// $redis->set($key, json_encode($list));
+
+$cache_data = $redis->get($key);
+
+$list = json_decode($cache_data);
+
+
+
+print_r($list);
+
+
 
 ?>
