024350c5bcb63cffc01f8b62caeb1143d49f9e28
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2025-11-20 09:21:31 +0700

    Update index_thang_25_4.php

--- admin/index_thang_25_4.php
+++ admin/index_thang_25_4.php
@@ -1,4 +1,9 @@
 <?php 
+
+    die;
+?>
+
+<?php 
 $a='23498423
 23538659
 37897283
@@ -15294,7 +15299,7 @@ foreach ($arrA as $index => $key) {
     $result[$key] = $arrB[$index] ?? null;
 }
 // --- Kết nối MySQL ---
-$mysqli = new mysqli("localhost", "sql_dienmay_bak", "bfsHT6wL4GBJnAYA", "sql_dienmay_bak");
+$mysqli = new mysqli("localhost", "username", "password", "database_name");
 $mysqli->set_charset("utf8");
 
 if ($mysqli->connect_errno) {
